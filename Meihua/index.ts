4833cc0cc1c59511ea28c4e65b209c7bfdf5757c
Suon Vansak <111901727+vansakweb@users.noreply.github.com>
2024-02-18 03:53:34 +0700

    Add files via upload

---
+++ Meihua/index.ts
@@ -0,0 +1,33 @@
+const meihua = [
+  {
+    title: "梅花（1）",
+    pdf: "Meihua/梅花_1",
+    cover: "Meihua/梅花_1.png",
+  },
+  {
+    title: "梅花（2）",
+    pdf: "Meihua/梅花_2",
+    cover: "Meihua/梅花_2.png",
+  },
+  {
+    title: "梅花（3）",
+    pdf: "Meihua/梅花_3",
+    cover: "Meihua/梅花_3.png",
+  },
+  {
+    title: "梅花（4）",
+    pdf: "Meihua/梅花_4",
+    cover: "Meihua/梅花_4.png",
+  },
+  {
+    title: "梅花（5）",
+    pdf: "Meihua/梅花_5",
+    cover: "Meihua/梅花_5.png",
+  },
+  {
+    title: "梅花（6）",
+    pdf: "Meihua/梅花_6",
+    cover: "Meihua/梅花_6.png",
+  },
+];
+export { meihua };
